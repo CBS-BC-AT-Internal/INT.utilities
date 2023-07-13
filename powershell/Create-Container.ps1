aed18d663c455c190aff1c27ecd941b12cb4a0de
cegekaJG <120664985+cegekaJG@users.noreply.github.com>
2023-07-13 07:54:54 +0000

    initial commit

---
+++ powershell/Create-Container.ps1
@@ -0,0 +1,56 @@
+﻿Param(
+    [string] $containerName = 'bcserver',
+    [string] $version,
+    [string] $auth = 'UserPassword',
+    [string] $licensePath,
+    [string] $shortCutPath = (Join-Path [Environment]::GetFolderPath("Desktop") $containerName),
+    [boolean] $includeTestToolkit
+)
+
+## === Get admin permissions
+
+Check-BcContainerHelperPermissions -Fix
+
+## === Build parameters
+
+$params = @{
+    "country" = 'at'
+    'select' = 'Latest'
+    'type' = 'onprem'
+}
+
+if ([string]::IsNullOrEmpty($version)) {
+    params += @{ 'version' = $version }
+}
+
+## === Fetch artifact URL
+
+Write-Host Get URL ArtiFact ...
+$artifactUrl = Get-BCArtifactUrl @params
+Write-Host Artifact URL = $artifactUrl -ForegroundColor DarkYellow
+
+## === Build parameters
+
+$params = @{
+    "accept_eula" = $true
+    "artifactUrl" = $artifactUrl
+    "auth" = $auth
+    "updateHosts" = $true
+    'includeTestToolkit' = $includeTestToolkit
+}
+
+if ([string]::IsNullOrEmpty($containerName)) {
+    $params += @{ "containerName" = $containerName }
+}
+if ([string]::IsNullOrEmpty($licensePath)) {
+    $params += @{ "licenseFile" = $licensePath }
+}
+if ([string]::IsNullOrEmpty($shortcutPath)) {
+    $params += @{ "shortcuts" = $shortcutPath }
+}
+
+## === Create Container
+
+Write-Host Creating Container ...
+
+New-BCContainer @params
